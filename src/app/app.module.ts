b8601937d890f9d0c6a76bc9dbaf90890e146ce8
meanxd <almaz.ahmatyanov12@gmail.com>
2023-02-22 17:30:13 +0300

    Changes:
    - Created placeholders
    - Created mock products BL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header/header.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product-list/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductListComponent,
+    ProductComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product-list/mock-products.ts
@@ -0,0 +1,8 @@
+import { Product } from "./product/product";
+
+export const PRODUCTS: Product[] =
+[
+    {title: 'Shoes', price: 10},
+    {title: 'Jacket', price: 100},
+    {title: 'Cap', price: 50},
+]
